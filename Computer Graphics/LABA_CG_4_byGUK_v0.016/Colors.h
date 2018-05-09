444f56dba83aa482153ad3a0385463ed097dcebd
CrazySquad <37076737+CrazySquad@users.noreply.github.com>
2018-05-09 19:14:38 +0700

    Add files via upload

---
+++ Computer Graphics/LABA_CG_4_byGUK_v0.016/Colors.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#define v5 RGB(75 ,10, 125) //Violet
+#define v4 RGB(83 ,23 ,129)   //5 степеней интенсивности
+#define v3 RGB(87 ,14, 143)
+#define v2 RGB(171 ,99, 227)
+#define v1 RGB(198, 160, 227)
+
+#define p5 RGB(142, 4, 97) //Pink
+#define p4 RGB(148, 17, 105)
+#define p3 RGB(162, 5, 111)
+#define p2 RGB(232, 92, 186)
+#define p1 RGB(232, 159, 208)
+
+#define b5 RGB(35, 16, 129)//Blue
+#define b4 RGB(48, 31, 134)
+#define b3 RGB(43, 22, 147)
+#define b2 RGB(127, 107, 228)
+#define b1 RGB(176, 165, 228)
+
+#define e5 RGB(186, 180, 5)//Yelow
+#define e4 RGB(194, 188, 23)
+#define e3 RGB(212, 206, 7)
+#define e2 RGB(244, 240, 96)
+#define e1 RGB(244, 242, 167)
